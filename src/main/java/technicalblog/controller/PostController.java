23460f60649a04d2f920efc1eba8d218f2fae0e0
Content Team <contentteam@UE-M-NOTE-0101.local>
2018-10-06 17:44:30 +0530

    Fourth milestone - JDBC code

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import technicalblog.model.Post;
 import technicalblog.service.PostService;
 
@@ -20,6 +21,17 @@ public class PostController {
         ArrayList<Post> posts = postService.getOnePost();
         model.addAttribute("posts", posts);
         return "posts";
+    }
+
+    @RequestMapping("/posts/newpost")
+    public String newPost() {
+        return "posts/create";
+    }
+
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    public String createPost(Post newPost) {
+        postService.createPost(newPost);
+        return "redirect:/posts";
     }
 
 }
--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,6 +3,7 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -16,24 +17,47 @@ public class PostService {
     public ArrayList<Post> getAllPosts() {
         ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
+//        Post post1 = new Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post Body 1");
+//        post1.setDate(new Date());
+//
+//        Post post2 = new Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post Body 2");
+//        post2.setDate(new Date());
+//
+//        Post post3 = new Post();
+//        post3.setTitle("Post 3");
+//        post3.setBody("Post Body 3");
+//        post3.setDate(new Date());
+//
+//        posts.add(post1);
+//        posts.add(post2);
+//        posts.add(post3);
 
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
 
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres","1234");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+            while(rs.next()) {
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
         return posts;
     }
@@ -41,13 +65,40 @@ public class PostService {
     public ArrayList<Post> getOnePost() {
         ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 = new Post();
-        post1.setTitle("This is your Post");
-        post1.setBody("This is your Post. It has some valid content");
-        post1.setDate(new Date());
-        posts.add(post1);
+//        Post post1 = new Post();
+//        post1.setTitle("This is your Post");
+//        post1.setBody("This is your Post. It has some valid content");
+//        post1.setDate(new Date());
+//        posts.add(post1);
+
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres","1234");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM posts WHERE id = 4");
+            while(rs.next()) {
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
         return posts;
+
+    }
+
+    public void createPost(Post newPost) {
 
     }
 
